3d078e3ef3a88cefb67ebea523b8505a2b0999e6
Mike Riley <mikehriley@hotmail.com>
2021-09-12 10:03:06 -0700

    For purposes of #define replacements, _ is considered alpha

--- asm.c
+++ asm.c
@@ -194,6 +194,7 @@ int isAlpha(char c) {
   if (c >= 'a' && c <= 'z') return -1;
   if (c >= 'A' && c <= 'Z') return -1;
   if (c >= '0' && c <= '9') return -1;
+  if (c >= '_') return -1;
   return 0;
   }
 
