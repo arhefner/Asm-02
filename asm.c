53a2178e1bba981eeb342e8d1423cdf4e3e922de
Tony Hefner <tony.hefner@gmail.com>
2023-09-06 18:30:42 -0400

    Update version number

--- asm.c
+++ asm.c
@@ -2361,7 +2361,7 @@ int main(int argc, char** argv) {
   int i;
   time_t tv;
   struct tm dt;
-  printf("Asm/02 v1.3\n");
+  printf("Asm/02 v1.4\n");
   printf("by Michael H. Riley\n");
   createLst = 0;
   outMode = 'R';
